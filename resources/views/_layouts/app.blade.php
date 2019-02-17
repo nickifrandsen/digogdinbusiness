2a96cde51b8fcc347aab690165919ac2c971c89a
Nicki Frandsen <nf@nickifrandsen.dk>
2019-02-17 16:13:02 +0700

    changed routes

--- resources/views/_layouts/app.blade.php
+++ resources/views/_layouts/app.blade.php
@@ -27,11 +27,10 @@
 	</div>
 	
 	<script src="/js/app.js"></script>
-	<script src="https://js.stripe.com/v3"></script>
-
-	<script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/9c05a6b2e7024f82f412a839f/faf61c151f4c9fad9a51eb72f.js");</script>
 
 	@yield('scripts')
+
+	<script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/9c05a6b2e7024f82f412a839f/faf61c151f4c9fad9a51eb72f.js");</script>
 
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -10,36 +10,33 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'WorkshopsController@index');
 
-Route::get('/home', function () {
+Route::get('home', function () {
     return view('welcome');
 });
-
-// Route::get('/', function () {
-//     return view('workshops.index');
-// });
-Route::get('/', 'WorkshopsController@index');
 Route::get('workshops', 'WorkshopsController@index');
 Route::get('workshops/tilmeldt', 'WorkshopsController@confirmed');
-Route::post('/workshops/{id}/tilmelding', 'WorkshopsController@store');
-Route::get('/betaling', 'PurchasesController@payment')->name('payment');
+Route::post('workshops/{id}/tilmelding', 'WorkshopsController@store');
 
-Route::get('/masterminds', function () {
+Route::get('betaling', 'PurchasesController@payment')->name('payment');
+
+Route::get('masterminds', function () {
     return view('masterminds');
 });
-Route::get('/sparring', function () {
+Route::get('sparring', function () {
     return view('oneonone');
 });
-Route::get('/om', function () {
+Route::get('om', function () {
     return view('about');
 });
-Route::get('/nyhedsbrev', function () {
+Route::get('nyhedsbrev', function () {
     return view('newsletter');
 });
-Route::get('/handelsbetingelser', function () {
+Route::get('handelsbetingelser', function () {
     return view('terms');
 });
-Route::get('/privatlivspolitik', function () {
+Route::get('privatlivspolitik', function () {
     return view('privacy');
 });
-Route::post('/purchases', 'PurchasesController@store');
+Route::post('purchases', 'PurchasesController@store');
